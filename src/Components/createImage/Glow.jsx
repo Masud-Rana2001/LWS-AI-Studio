b17ae3daf6b6e6bad2a565e00fcc403446bd312a
Masud Rana <mdmasudrana307801@gmail.com>
2025-10-04 15:56:05 +0600

    Fixing bug

--- src/Components/createImage/Glow.jsx
+++ src/Components/createImage/Glow.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Glow() {
   return (
-    <div className="fixed -bottom-1/4 -right-0 ">
+    <div className="fixed -bottom-1/4 -right-0  -z-10">
   <div className="h-96 w-96 bg-gradient-to-r from-pink-600 to-indigo-400 rotate-90 rounded-full blur-[180px] "></div>
 </div>
 
